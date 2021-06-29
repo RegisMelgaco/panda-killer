d07cc9e2a37216bcd8cecf8c7a968d05be2ac18a
Régis Melgaço de Andrade <regis.melgaco@gmai.com>
2021-06-29 15:38:20 -0300

    Refactor rest responses.

--- pkg/gateway/rest/response.go
+++ pkg/gateway/rest/response.go
@@ -1,5 +1,23 @@
 package rest
 
+type AccountBalanceResponse struct {
+	Balance float64 `json:"balance"`
+}
+
+type ErrorResponse struct {
+	Message string
+}
+
+type CreatedAccountResponse struct {
+	ID int `json:"id"`
+}
+
+type GetAccountResponse struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	CPF  string `json:"cpf"`
+}
+
 type CreateTransferResponse struct {
 	ID int `json:"id"`
 }
