5c3d4eb6516b79c99c51759630b3e998df006242
Régis Melgaço de Andrade <regis.melgaco@gmai.com>
2021-06-29 11:19:01 -0300

    Comments for domain errors.

--- pkg/domain/entity/account/error.go
+++ pkg/domain/entity/account/error.go
@@ -2,6 +2,11 @@ package account
 
 import "errors"
 
+// Is used when the account name is empty.
 var ErrAccountNameIsObligatory error = errors.New("Account name is Obligatory")
+
+// Is used when the account cpf doesn't have a length equals to 11.
 var ErrAccountCPFShouldHaveLength11 error = errors.New("Account cpf should have length 11")
+
+// Is used when trying to use a non existing account.
 var ErrAccountNotFound error = errors.New("Account not found")
