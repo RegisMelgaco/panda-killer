3e3313d5b4859f1823d52df7b79edb64a0130268
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2021-07-23 18:00:43 -0300

    Unit tests for accountUsecase.

--- pkg/domain/entity/account/repository.go
+++ pkg/domain/entity/account/repository.go
@@ -2,6 +2,8 @@ package account
 
 import "context"
 
+//go:generate moq -out repository_mock.go . AccountRepo
+
 type AccountRepo interface {
 	CreateAccount(context.Context, *Account) error
 	GetAccounts(context.Context) ([]Account, error)
---
+++ pkg/domain/usecase/account_test.go
@@ -0,0 +1,80 @@
+package usecase_test
+
+import (
+	"context"
+	"errors"
+	"local/panda-killer/pkg/domain/entity/account"
+	"local/panda-killer/pkg/domain/usecase"
+	"local/panda-killer/pkg/gateway/algorithms"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type accountRepoMock struct {
+	CreateAccount   func(context.Context, *account.Account) error
+	GetAccounts     func(context.Context) ([]account.Account, error)
+	GetAccount      func(context.Context, account.AccountID) (*account.Account, error)
+	GetAccountByCPF func(ctx context.Context, cpf string) (*account.Account, error)
+}
+
+func TestGetAccounts(t *testing.T) {
+	cases := []struct {
+		name        string
+		mockedRepo  account.AccountRepo
+		expected    []account.Account
+		expectedErr error
+	}{
+		{
+			name: "with repo mock without error should retrieve accounts from mock",
+			mockedRepo: &account.AccountRepoMock{
+				GetAccountsFunc: func(contextMoqParam context.Context) ([]account.Account, error) {
+					return []account.Account{
+						{
+							ID:      1,
+							Balance: 10,
+							Name:    "Bruna",
+							CPF:     "12345678912",
+							Secret:  "78312798419",
+						},
+					}, nil
+				},
+			},
+			expected: []account.Account{
+				{
+					ID:      1,
+					Balance: 10,
+					Name:    "Bruna",
+					CPF:     "12345678912",
+					Secret:  "78312798419",
+				},
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "with repo mock with error should retrieve the error",
+			mockedRepo: &account.AccountRepoMock{
+				GetAccountsFunc: func(contextMoqParam context.Context) ([]account.Account, error) {
+					return nil, errors.New("Internal error")
+				},
+			},
+			expected:    nil,
+			expectedErr: errors.New("Internal error"),
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			uc := usecase.NewAccountUsecase(
+				c.mockedRepo, algorithms.PasswordHashingAlgorithmsImpl{},
+			)
+
+			accounts, err := uc.GetAccounts(context.Background())
+
+			assert.Equal(t, c.expected, accounts)
+			assert.Equal(t, c.expectedErr, err)
+		})
+	}
+}
